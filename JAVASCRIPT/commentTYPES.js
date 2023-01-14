5c49d53ba012e9026e58a02c40b8f6e2243a1e7c
samshu33 <samadhanpradhan33@gmail.com>
2023-01-14 18:05:35 +0530

    s

---
+++ JAVASCRIPT/commentTYPES.js
@@ -0,0 +1,16 @@
+
+// single line comment start
+    // lwowhi hello by dont ServiceWorkerContainer; a f ahnlkndc bfaanda cnaksanfffbfa,djna,n,md,nbbjkakfnakdwddjsakjasknkndknakdnakjnkankjassnknsana
+// single line comment end
+
+
+sok
+// double line comment is on
+// hiijijlkgbkjbnlhgij
+// historyk
+// KeyboardEvent
+// false
+// KeyboardEvent
+// matchMedia
+// debugger
+ho
---
+++ JAVASCRIPT/typesOfop.js
@@ -0,0 +1,4 @@
+console.log("SAMADHAN")
+document.write("samadhan")
+alert("samadhan")
+document.getElementById('p').innerHTML="inner html"
